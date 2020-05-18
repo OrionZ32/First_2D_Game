32fb774c6b228afe1402f63ec5f9030ac1a4d672
Harley Garcia <hagarcia99@outlook.com>
2020-05-18 14:08:12 -0500

    Getting story text to generate with scriptable object

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    void Start() {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    void Update() {
+        
+    }
+}
