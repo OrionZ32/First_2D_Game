254fa92ac0e9192bb7e49834712fffc5ae473274
Harley Garcia <hagarcia99@outlook.com>
2020-05-19 08:14:59 -0500

    Final commit for short adventure game

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -21,12 +21,12 @@ public class AdventureGame : MonoBehaviour {
 
     private void ManageState() {
         var nextStates = state.GetNextStates();
-        
-        if(Input.GetKeyDown(KeyCode.Alpha1)) {
-            state = nextStates[0];
-        }
-        else if(Input.GetKeyDown(KeyCode.Alpha2)) {
-            state = nextStates[1];
+
+        for (int index = 0; index < nextStates.Length; index++) {
+            
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index)) {
+                state = nextStates[index];
+            }
         }
 
         textComponent.text = state.GetStateStory();
