ec47a402bc77924e10322f0c2709b3911c067d78
Harley Garcia <hagarcia99@outlook.com>
2020-05-18 21:51:24 -0500

    Setting up player input and choices

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -16,6 +16,19 @@ public class AdventureGame : MonoBehaviour {
     }
 
     void Update() {
+        ManageState();
+    }
+
+    private void ManageState() {
+        var nextStates = state.GetNextStates();
         
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2)) {
+            state = nextStates[1];
+        }
+
+        textComponent.text = state.GetStateStory();
     }
 }
